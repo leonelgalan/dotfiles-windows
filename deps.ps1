f97fe880d7c117e77cf9cbd2e94f1967886751be
Leonel Galan User <leonelgalan@gmail.com>
2020-08-27 22:24:27 +0000

    Don't install Janus
    I don't know what Janus is

--- deps.ps1
+++ deps.ps1
@@ -61,9 +61,3 @@ nvm use $nodeLtsVersion
 Remove-Variable nodeLtsVersion
 
 gem pristine --all --env-shebang
-### Janus for vim
-Write-Host "Installing Janus..." -ForegroundColor "Yellow"
-if ((which curl) -and (which vim) -and (which rake) -and (which bash)) {
-    curl.exe -L https://bit.ly/janus-bootstrap | bash
-}
-
